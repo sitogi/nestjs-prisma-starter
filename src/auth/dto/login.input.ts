e305cd174235f7cfed9f51905c8ca5039d69e867
sitogi <takano162@gmail.com>
2022-10-08 22:05:03 +0900

    Add some decorators of swagger

--- src/auth/dto/login.input.ts
+++ src/auth/dto/login.input.ts
@@ -1,9 +1,19 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginInput {
+  @ApiProperty({
+    description: 'email',
+    default: 'test@example.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'password',
+    minimum: 8,
+    default: '12345678',
+  })
   @IsNotEmpty()
   @MinLength(8)
   password: string;
--- src/auth/models/token.model.ts
+++ src/auth/models/token.model.ts
@@ -1,4 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class Token {
+  @ApiProperty({
+    description: 'accessToken',
+  })
   accessToken: string;
+
+  @ApiProperty({
+    description: 'refreshToken',
+  })
   refreshToken: string;
 }
